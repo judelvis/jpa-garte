8553efda1030650d8e4758f25392cf2f4893ff40
judelvis <jud.prog@gmail.com>
2015-04-25 18:38:53 -0430

    prueba de galeria

--- application/views/panel/creaGaleria.php
+++ application/views/panel/creaGaleria.php
@@ -17,19 +17,43 @@
 			<div id="div-1" class="accordion-body collapse in body">
 				<form class="form-horizontal">
 					<div class="form-group">
-						<label for="autosize" class="control-label col-lg-2">Codigo</label>
-						<div class="col-lg-10">
+						<label for="autosize" class="control-label col-lg-2">Serie</label>
+						<div class="col-lg-4">
 							<?php if(isset($id)){?>
-							<input type="text" placeholder="Codigo" class="form-control"
-								name="inmueble" id="inmueble" value='<?php echo $id;?>' />
+							<input type="text" placeholder="Serie" class="form-control"
+								name="serie" id="serie" value='<?php echo $id;?>' />
 							<?php }else{?>
-							<select data-placeholder="Seleccione Inmueble"
-								class="form-control " id='inmueble' name='inmueble'
+							<select data-placeholder="Seleccione Serie"
+								class="form-control " id='serie' name='serie'
 								onchange="consultar();"></select>
 							<?php }?>
-
+                        </div>
+                        <label for="autosize" class="control-label col-lg-2">Categoria</label>
+                        <div class="col-lg-4">
+                                <select data-placeholder="Seleccione categoria"
+                                        class="form-control " id='categoria' name='categoria'></select>
 						</div>
 					</div>
+
+                    <div class="form-group">
+                        <label for="autosize" class="control-label col-lg-2">Titulo</label>
+                        <div class="col-lg-4">
+                                <input type="text" placeholder="Titulo" class="form-control"
+                                       name="titulo" id="titulo"  />
+                        </div>
+                        <label for="autosize" class="control-label col-lg-2">Fecha</label>
+                        <div class="col-lg-4">
+                            <input type="text" placeholder="Fecha" class="form-control"
+                                   name="fecha" id="fecha"  />
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="autosize" class="control-label col-lg-2">Detalle</label>
+                        <div class="col-lg-10">
+                            <textarea id="detalle" name="detalle" class="form-control"></textarea>
+                        </div>
+                    </div>
 
 					<div class="form-group">
 						<label class="control-label col-lg-4">Seleccione Imagen</label>
